e2d097998cbcdd3344c68ba446eab8b245beb803
Pallabsaikia <psaikia2@zaloni.com>
2022-11-05 11:46:49 +0530

    Self collision bugs fixed

--- functions.py
+++ functions.py
@@ -1,126 +1,120 @@
-
-
-def change_auto_direction(mysnake,fruit,mysnake_dir):
+def change_auto_direction(mysnake, fruit, mysnake_dir, dis_width):
     if mysnake[0][0] == fruit[0]:
-       if mysnake[0][1] < fruit[1]:
-          if mysnake_dir !='UP':
-             return 'DOWN'
-          else:
-             return 'LEFT' 
-       else:
-          if mysnake_dir != 'DOWN':
-            return 'UP'
-          else:
-            return 'LEFT' 
-    elif mysnake[0][1] == fruit[1]:  
+        if mysnake[0][1] < fruit[1]:
+            if mysnake_dir != 'UP':
+                return 'DOWN'
+            else:
+                if mysnake[0][0] != 0:
+                    return 'LEFT'
+                else:
+                    return 'RIGHT'
+        else:
+            if mysnake_dir != 'DOWN':
+                return 'UP'
+            else:
+                if mysnake[0][0] != 0:
+                    return 'LEFT'
+                else:
+                    return 'RIGHT'
+    elif mysnake[0][1] == fruit[1]:
         if mysnake[0][0] < fruit[0]:
-            if mysnake_dir !='LEFT':
+            if mysnake_dir != 'LEFT':
                 return 'RIGHT'
             else:
-                return 'UP'
+                if mysnake[0][1] != 0:
+                    return 'UP'
+                else:
+                    return 'DOWN'
         else:
-            if mysnake_dir !='RIGHT':
+            if mysnake_dir != 'RIGHT':
                 return 'LEFT'
             else:
-                return 'UP'
-    elif mysnake[0][1]  < fruit[1] and mysnake[0][1] < fruit[1]:
+                if mysnake[0][1] != 0:
+                    return 'UP'
+                else:
+                    return 'DOWN'
+    elif mysnake[0][1] < fruit[1] and mysnake[0][1] < fruit[1]:
         if mysnake_dir != 'LEFT':
-            return 'RIGHT'  
-        else:
-            return 'DOWN'
-    elif mysnake[0][1] < fruit[1] and mysnake[0][1] > fruit[1]: 
-        if mysnake_dir !='LEFT':
             return 'RIGHT'
         else:
-            return 'UP'   
-    elif mysnake[0][1]  > fruit[1] and mysnake[0][1] < fruit[1]: 
+            return 'DOWN'
+    elif mysnake[0][1] < fruit[1] < mysnake[0][1]:
+        if mysnake_dir != 'LEFT':
+            return 'RIGHT'
+        else:
+            return 'UP'
+    elif mysnake[0][1] > fruit[1] > mysnake[0][1]:
         if mysnake_dir != 'RIGHT':
             return 'LEFT'
         else:
             return 'DOWN'
-    elif mysnake[0][1]  > fruit[1] and mysnake[0][1] > fruit[1]: 
+    elif mysnake[0][1] > fruit[1] and mysnake[0][1] > fruit[1]:
         if mysnake_dir != 'RIGHT':
             return 'LEFT'
         else:
-            return 'UP'          
+            return 'UP'
+
+    # return check_before_self_collision(mysnake, dis_width, fruit, mysnake_dir)
 
 
-def check_before_self_collision(mysnake,dis_width,fruit,mysnake_dir):
+def check_before_self_collision(mysnake, dis_width, fruit, mysnake_dir):
     for block in mysnake[1:]:
         if mysnake_dir == "UP":
             diff = mysnake[0][1] - block[1]
-            print(diff)
             if 0 < diff <= 20 and mysnake[0][0] == block[0]:
                 if mysnake[0][0] < fruit[0]:
-                    print("right")
-                    if not is_snake_collision_right(mysnake,dis_width):
+                    if not is_snake_collision_right(mysnake, dis_width):
                         return 'RIGHT'
-                        
                     else:
                         return 'LEFT'
-                    
                 else:
                     print("left")
-                    if not is_snake_collision_left(mysnake,dis_width):
+                    if not is_snake_collision_left(mysnake):
                         return 'LEFT'
-                        
                     else:
                         return 'RIGHT'
+        elif mysnake_dir == "DOWN":
+            diff = block[1] - mysnake[0][1]
+            if 0 < diff <= 20 and mysnake[0][0] == block[0]:
+                if mysnake[0][0] < fruit[0]:
+                    if not is_snake_collision_right(mysnake, dis_width):
+                        return 'RIGHT'
+                    else:
+                        return 'LEFT'
+                else:
+                    if not is_snake_collision_left(mysnake):
+                        return 'LEFT'
+                    else:
+                        return 'RIGHT'
+        elif mysnake_dir == "LEFT":
+            diff = mysnake[0][0] - block[0]
+            if 0 < diff <= 20 and mysnake[0][1] == block[1]:
+                if mysnake[0][1] < fruit[1]:
+                    if not is_snake_collision_down(mysnake, dis_width):
+                        return 'DOWN'
+                    else:
+                        return 'UP'
+                else:
+                    if not is_snake_collision_up(mysnake):
+                        return 'UP'
+                    else:
+                        return 'DOWN'
+        elif mysnake_dir == "RIGHT":
+            diff = block[0] - mysnake[0][0]
+            if 0 < diff <= 20 and mysnake[0][1] == block[1]:
+                if mysnake[0][1] < fruit[1]:
+                    if not is_snake_collision_down(mysnake, dis_width):
+                        return 'DOWN'
+                    else:
+                        return 'UP'
+                else:
+                    if not is_snake_collision_up(mysnake):
+                        return 'UP'
+                    else:
+                        return 'DOWN'
 
-            elif mysnake_dir == "DOWN":
-                diff = block[1] - mysnake[0][1]
-                if 0 < diff <= 2 and mysnake[0][0] == block[0]:
-                    if mysnake[0][0] < fruit[0]:
-                        if not is_snake_collision_right(mysnake,dis_width):
-                            return 'RIGHT'
-                            
-                        else:
-                            return 'LEFT'
-                            
-                    else:
-                        if not is_snake_collision_left(mysnake,dis_width):
-                            return 'LEFT'
-                            
-                        else:
-                            return 'RIGHT'
-                            
-            elif mysnake_dir == "LEFT":
-                diff = mysnake[0][0] - block[0]
-                if 0 < diff <= 20 and mysnake[0][1] == block[0]:
-                    if mysnake[0][1] < fruit[1]:
-                        if not is_snake_collision_down(mysnake,dis_width):
-                            return 'DOWN'
-                            
-                        else:
-                            return 'UP'
-                            
-                    else:
-                        if not is_snake_collision_up(mysnake,dis_width):
-                            return 'UP'
-                            
-                        else:
-                            return 'DOWN'
-                            
-            elif mysnake_dir == "RIGHT":
-                diff = block[0] - mysnake[0][0]
-                if 0 < diff <= 20 and mysnake[0][1] == block[0]:
-                    if mysnake[0][1] < fruit[1]:
-                        if not is_snake_collision_down(mysnake,dis_width):
-                            return 'DOWN'
-                            
-                        else:
-                            return 'UP'
-                            
-                    else:
-                        if not is_snake_collision_up(mysnake,dis_width):
-                            return 'UP'
-                            
-                        else:
-                            return 'DOWN'
-                            
-                        
 
-def is_snake_collision_right(mysnake,dis_width):
+def is_snake_collision_right(mysnake, dis_width):
     my_x_pos = int(mysnake[0][0])
     my_y_pos = int(mysnake[0][1])
     while my_x_pos < dis_width:
@@ -128,9 +122,10 @@ def is_snake_collision_right(mysnake,dis_width):
             if block[0] == my_x_pos and block[1] == my_y_pos:
                 return True
         my_x_pos += 1
-    return False    
+    return False
 
-def is_snake_collision_left(mysnake,dis_width):
+
+def is_snake_collision_left(mysnake):
     my_x_pos = int(mysnake[0][0])
     my_y_pos = int(mysnake[0][1])
     while my_x_pos > 0:
@@ -140,22 +135,24 @@ def is_snake_collision_left(mysnake,dis_width):
         my_x_pos -= 1
     return False
 
-def is_snake_collision_up(mysnake):
-        my_x_pos = int(mysnake[0][0])
-        my_y_pos = int(mysnake[0][1])
-        while my_y_pos > 0:
-            for block in mysnake[1:]:
-                if block.y == my_y_pos and block.x == my_x_pos:
-                    return True
-            my_y_pos -= 1
-        return False
 
-def is_snake_collision_down(mysnake,dis_width):
+def is_snake_collision_up(mysnake):
+    my_x_pos = int(mysnake[0][0])
+    my_y_pos = int(mysnake[0][1])
+    while my_y_pos > 0:
+        for block in mysnake[1:]:
+            if block[1] == my_y_pos and block[0] == my_x_pos:
+                return True
+        my_y_pos -= 1
+    return False
+
+
+def is_snake_collision_down(mysnake, dis_width):
     my_x_pos = int(mysnake[0][0])
     my_y_pos = int(mysnake[0][1])
     while my_y_pos < dis_width:
         for block in mysnake[1:]:
-            if block.y == my_y_pos and block.x == my_x_pos:
+            if block[1] == my_y_pos and block[0] == my_x_pos:
                 return True
         my_y_pos += 1
-    return False         
+    return False
--- teams.py
+++ teams.py
@@ -1,19 +1,16 @@
-import imp
-import math 
 from functions import *
 
-def alpha( mysnake, othersnake, fruit, mysnake_dir, othersnake_dir, mysnake_pos, othersnake_pos, dis_width, dis_height ):
 
-    new_direction = mysnake_dir 
+def alpha(mysnake, othersnake, fruit, mysnake_dir, othersnake_dir, mysnake_pos, othersnake_pos, dis_width, dis_height):
+    new_direction = mysnake_dir
     mysnake_new_position_x = mysnake_pos[0]
     mysnake_new_position_y = mysnake_pos[1]
-    
-    #print(fruit)
-    
-    #new_direction = change_auto_direction(mysnake,fruit,mysnake_dir)
 
-    new_direction = check_before_self_collision(mysnake,dis_width,fruit,mysnake_dir)
-	# Moving the snakes
+    # new_direction = check_before_self_collision(mysnake, dis_width, fruit, mysnake_dir)
+    # if not new_direction:
+    new_direction = change_auto_direction(mysnake, fruit, mysnake_dir, dis_width)
+
+    # Moving the snakes
     if new_direction == 'UP':
         mysnake_new_position_y -= 10
     if new_direction == 'DOWN':
@@ -24,34 +21,33 @@ def alpha( mysnake, othersnake, fruit, mysnake_dir, othersnake_dir, mysnake_pos,
         mysnake_new_position_x += 10
 
     # avoid left wall 
-    if mysnake_new_position_x < 0: #hitting left wall 
-       new_direction = 'UP'
+    if mysnake_new_position_x < 0:  # hitting left wall
+        new_direction = 'UP'
 
     # avoid left wall 
-    if mysnake_new_position_x > dis_width-10 and mysnake_new_position_y < fruit[1]: #hitting left wall 
-       new_direction = 'DOWN'
-   
+    if mysnake_new_position_x > dis_width - 10 and mysnake_new_position_y < fruit[1]:  # hitting left wall
+        new_direction = 'DOWN'
+
     # avoid left right 
-    elif mysnake_new_position_x > dis_width-10: #hitting left wall 
-       new_direction = 'UP'
-    
+    elif mysnake_new_position_x > dis_width - 10:  # hitting left wall
+        new_direction = 'UP'
+
     # avoid left wall 
-    #if mysnake_new_position_y > dis_height-10: #hitting left wall 
+    # if mysnake_new_position_y > dis_height-10: #hitting left wall
     #   new_direction = 'LEFT'
     # avoid up wall
-    #if mysnake_new_position_y < 0: #hitting left wall 
+    # if mysnake_new_position_y < 0: #hitting left wall
     #   new_direction = 'LEFT'   
-		
+
     return new_direction
 
-def beta( mysnake, othersnake, fruit, mysnake_dir, othersnake_dir, mysnake_pos, othersnake_pos, dis_width, dis_height ):
 
-    new_direction = mysnake_dir 
+def beta(mysnake, othersnake, fruit, mysnake_dir, othersnake_dir, mysnake_pos, othersnake_pos, dis_width, dis_height):
+    new_direction = mysnake_dir
     mysnake_new_position_x = mysnake_pos[0]
     mysnake_new_position_y = mysnake_pos[1]
-    
 
-	# Moving the snakes
+    # Moving the snakes
     if mysnake_dir == 'UP':
         mysnake_new_position_y -= 10
     if mysnake_dir == 'DOWN':
@@ -62,17 +58,16 @@ def beta( mysnake, othersnake, fruit, mysnake_dir, othersnake_dir, mysnake_pos, 
         mysnake_new_position_x += 10
 
     # avoid left wall 
-    if mysnake_new_position_x < 0: #hitting left wall 
-       new_direction = 'UP'
+    if mysnake_new_position_x < 0:  # hitting left wall
+        new_direction = 'UP'
     # avoid left right 
-    if mysnake_new_position_x > dis_width-10: #hitting left wall 
-       new_direction = 'UP'
+    if mysnake_new_position_x > dis_width - 10:  # hitting left wall
+        new_direction = 'UP'
     # avoid left wall 
-    if mysnake_new_position_y < 0: #hitting left wall 
-       new_direction = 'LEFT'
+    if mysnake_new_position_y < 0:  # hitting left wall
+        new_direction = 'LEFT'
     # avoid left wall 
-    if mysnake_new_position_y > dis_height-10: #hitting left wall 
-       new_direction = 'LEFT'
-		
-		
+    if mysnake_new_position_y > dis_height - 10:  # hitting left wall
+        new_direction = 'LEFT'
+
     return new_direction
