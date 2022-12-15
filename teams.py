32a5068a114db4bcc292e97ee802a79dc9f736dd
palash-shome <palashshome09@gmail.com>
2022-12-15 14:39:16 +0530

    Updated from code byte

--- teams.py
+++ teams.py
@@ -1,5 +1,6 @@
 from copperhead import *
 from side_winder_func import *
+from codebyte import *
 
 # def alpha(mysnake, othersnake, fruit, mysnake_dir, othersnake_dir, mysnake_pos, othersnake_pos, dis_width, dis_height):
 #     new_direction = mysnake_dir
